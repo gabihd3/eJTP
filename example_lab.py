d3e9076ad8dc04441b75f7c8c828f240fb67fbc7
gabihd3 <gabi.jdh@gmail.com>
2022-10-30 14:51:09 -0400

    first section

--- example_lab.py
+++ example_lab.py
@@ -1,3 +1,21 @@
 import requests
 import json 
 import re
+
+port = 80
+dest = 'localhost'
+
+txt1 = 'http:/{endpoint}:{p}'.format(endpoint = dest, p = port)
+request = requests.get(txt1)
+print(request)
+
+
+dic = requests.headers
+dic = str(dic)
+print(dic)
+
+
+server = re.findall("'Server:' '([a-zA-Z0-9)\/\.{1,}]' ", dic)
+print(server)
+
+
